2f87481209e61afb37bc091753f9db53c8fca4a6
puakehaulani <puakehaulani@gmail.com>
2020-11-11 23:04:32 -0800

    added in express

--- routes/htmlRoute.js
+++ routes/htmlRoute.js
@@ -1,11 +1,13 @@
-var path = require("path");
+const path = require("path");
+const express = require("express");
+const router = express.Router();
 
-module.exports = function (app) {
-    app.get("/notes", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/notes.html"));
-    });
+router.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
+});
 
-    app.get("*", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/index.html"));
-    });
-};
+router.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+module.exports = router
