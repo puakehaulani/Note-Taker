433ff9ce01402433540dcf5c97d1febebc5587ff
puakehaulani <puakehaulani@gmail.com>
2020-11-11 23:04:11 -0800

    added in get and post

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,16 +1,30 @@
 const fs = require("fs");
+const path = require("path");
+const express = require("express");
+const router = express.Router();
 
+router.get("/", function (req, res) {
+    const databuffer = fs.readFileSync(path.join(__dirname, "../db/db.json"));
+    const dataJson = databuffer.toString();
+    const dataParse = JSON.parse(dataJson);
+    res.json(dataParse);
 
-module.exports = function (app) {
-    app.get("/api/notes", function (req, res) {
+});
+router.post("/", function (req, res) {
+    const databuffer = fs.readFileSync(path.join(__dirname, "../db/db.json"));
+    const dataJson = databuffer.toString();
+    const dataParse = JSON.parse(dataJson);
+    const newNote = { "title": req.body.title, "text": req.body.text };
+    dataParse.push(newNote);
+    fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(dataParse), err => {
+        if (err) throw err;
+        console.log("New note added to database!");
+    })
+    res.send(newNote);
+});
 
-    });
-    app.post("/api/notes", function (req, res) {
+// app.delete("/:id", function (req, res) {
 
-    });
+// });
 
-    app.delete("/api/notes/:id", function (req, res) {
-
-    });
-
-}
+module.exports = router
