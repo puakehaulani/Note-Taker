b56cadd181b9ad77d7403b9a2351664991e1d788
puakehaulani <puakehaulani@gmail.com>
2020-11-11 20:56:02 -0800

    restarted structure of api routes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+
+
+module.exports = function (app) {
+    app.get("/api/notes", function (req, res) {
+
+    });
+    app.post("/api/notes", function (req, res) {
+
+    });
+
+    app.delete("/api/notes/:id", function (req, res) {
+
+    });
+
+}
