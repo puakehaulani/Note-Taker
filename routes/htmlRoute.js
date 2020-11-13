9c2331b9788bee3072b47f201f86b3def43c3cf9
puakehaulani <puakehaulani@gmail.com>
2020-11-13 14:35:34 -0800

    added '/' route for heroku

--- routes/htmlRoute.js
+++ routes/htmlRoute.js
@@ -5,7 +5,9 @@ const router = express.Router();
 router.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
-
+router.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 router.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
 });
